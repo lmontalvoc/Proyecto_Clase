1b1dcdbc50bd86c5eea8533ab61edb0092fe713a
fernandop-dev <fernandopuertof@gmail.com>
2025-11-23 22:57:07 -0600

    Fix boton historial

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
 import "react-native-get-random-values";
 import React from "react";
 
-import RootNavigator from "./src/navigation/RootNavigator";
 import { Provider } from "react-redux";
 import store from "./src/store";
-
 import { ThemeProvider } from "./src/theme/ThemeContext";
+import RootNavigator from "./src/navigation/RootNavigator";
 
 export default function App() {
   return (
